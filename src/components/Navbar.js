24f87efcb8f5b1fb03d0ec8ff22657097d414bea
wxp02 <wxp0025@gmail.com>
2024-04-01 16:10:30 -0400

    First iteration

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header className="bg-gray-800 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
+          <a href="#about" className="ml-3 text-xl">
+            Wenxu Poh
+          </a>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#projects" className="mr-5 hover:text-white">
+            Past Work
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Skills
+          </a>
+          <a href="#testimonials" className="mr-5 hover:text-white">
+            Testimonials
+          </a>
+        </nav>
+        {/* Add LinkedIn link here */}
+        <a
+          href="https://www.linkedin.com/in/wenxu-p-1833291ab/"
+          src="./others/linkedin.svg"
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        >
+          <img
+            src="./others/linkedin.svg"
+            alt="LinkedIn"
+            className="w-6 h-6 mr-1.5"
+          />
+          LinkedIn
+        </a>
+        {/* Add GitHub link here */}
+        <a
+          href="https://github.com/wxp02"
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0 ml-4" // Added ml-4 for spacing
+        >
+          <img
+            src="./others/github.svg"
+            alt="GitHub"
+            className="w-6 h-6 mr-1.5"
+          />
+          GitHub
+        </a>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { skills } from "../data";
+import { ChipIcon } from "@heroicons/react/solid";
+
+export default function Skills() {
+  const renderSkillBadge = (skill) => (
+    <div key={skill} className="p-2">
+      <div className="bg-gray-800 rounded flex items-center p-4 h-full justify-center">
+        <img
+          alt={`${skill} icon`}
+          src={`./software/${skill.toLowerCase()}.svg`}
+          className="w-6 h-6 flex-shrink-0 mr-4"
+        />
+        <span className="title-font font-medium text-white">{skill}</span>
+      </div>
+    </div>
+  );
+
+  return (
+    <section id="skills" className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto">
+        {/* Skills Heading */}
+        <div className="text-center mb-20">
+          <ChipIcon className="w-10 inline-block mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+            Skills
+          </h1>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+            These are the tools and technologies I have learnt so far.
+          </p>
+        </div>
+        {/* Languages Section */}
+        <div className="mb-10">
+          <h2 className="text-center title-font font-medium text-white tracking-widest text-md mb-4">
+            Languages
+          </h2>
+          <div className="flex flex-wrap justify-center -mx-2">
+            {skills.languages.map(renderSkillBadge)}
+          </div>
+        </div>
+
+        {/* Frameworks Section */}
+        <div className="mb-10">
+          <h2 className="text-center title-font font-medium text-white tracking-widest text-md mb-4">
+            Frameworks
+          </h2>
+          <div className="flex flex-wrap justify-center -mx-2">
+            {skills.frameworks.map(renderSkillBadge)}
+          </div>
+        </div>
+
+        {/* Technologies Section */}
+        <div className="mb-10">
+          <h2 className="text-center title-font font-medium text-white tracking-widest text-md mb-4">
+            Technologies
+          </h2>
+          <div className="flex flex-wrap justify-center -mx-2">
+            {skills.technologies.map(renderSkillBadge)}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
